f936303299a9753bf93f0bcdcc759936930f8ac4
Max Hutchinson <max.hutchinson@sse.com>
2022-10-10 15:27:00 +0100

    Added Demo OpenTelemetry

--- EventIngester/Program.cs
+++ EventIngester/Program.cs
@@ -1,7 +1,58 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Ingester.Middleware;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Trace;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .Build();
+    .ConfigureFunctionsWorkerDefaults(WorkerApp =>
+    {
+        WorkerApp.UseMiddleware<ExceptionLoggingMiddleware>();
+    })
+
+.ConfigureServices(builder =>
+{
+    var azureMonitorInstrumentationKey = Environment.GetEnvironmentVariable(@"APPINSIGHTS_INSTRUMENTATIONKEY");
+    if (!string.IsNullOrWhiteSpace(azureMonitorInstrumentationKey))
+    {
+        /// <remarks>
+        /// Logs will be ingested as an Application Insights trace.
+        /// These can be differentiated by their severityLevel.
+        /// </remarks>
+        builder.AddOpenTelemetryTracing((t) => t
+
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            //.AddSource(nameof(CloudEventIngester)) // add services like this
+            .AddAzureMonitorTraceExporter(o =>
+            {
+                o.ConnectionString = azureMonitorInstrumentationKey;
+            }));
+
+        /// <remarks>
+        /// These counters will be aggregated and ingested as Application Insights customMetrics.
+        /// </remarks>
+        builder.AddOpenTelemetryMetrics((m) => m
+
+            //.AddMeter("MyCompany.MyProduct.MyLibrary")
+            .AddAzureMonitorMetricExporter(o =>
+            {
+                o.ConnectionString = azureMonitorInstrumentationKey;
+            }));
+
+        ///// <remarks>
+        ///// Logs will be ingested as an Application Insights trace.
+        ///// These can be differentiated by their severityLevel.
+        ///// </remarks>
+        //loggerFactory = LoggerFactory.Create(builder =>
+        //{))
+        //    builder.AddOpenTelemetry(options =>
+        //    {
+        //        options.AddAzureMonitorLogExporter(o => o.ConnectionString = azureMonitorInstrumentationKey);
+        //    });
+        //});
+    }
+}).Build();
 
 host.Run();
